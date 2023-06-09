ec29d8f5f90fa32e4d29421e054e1c00d7e13fe7
TomasNagy <tomi.nagy@outlook.com>
2023-06-09 20:30:04 +0200

    feat: implement min and max functions

--- find_min_max.c
+++ find_min_max.c
@@ -1,9 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int get_numbers(int array_to_save[]);
+int min(int numbers[], int array_length);
+int max(int numbers[], int array_length);
 
 int main(int argc, char *argv[])
 {
-    const int elements = get_int("Number of elements: ");
+    if (argc != 2)
+    {
+        printf ("Usage: ./find_min_max <min>\n");
+        return EXIT_FAILURE;
+    }
+    
+    int input;
+    do
+    {
+       input = get_int("Number of elements: ");
+    } 
+    while (input <= 0 || input >= 15);
+
+    const int elements = input;
 
     int numbers[elements];
 
@@ -11,5 +30,47 @@ int main(int argc, char *argv[])
     {
         numbers[i] = get_int("Element %i: ", i+1);
     }
-    
+
+    if (strcmp("min", argv[1]) == 0)
+    {
+        printf ("The min value is: %i\n", min(numbers, elements));
+        return EXIT_SUCCESS;
+    }
+    else if (strcmp("max", argv[1]) == 0)
+    {
+        printf ("The max value is: %i\n", max(numbers, elements));
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_FAILURE;
+}
+
+int min(int numbers[], int array_length)
+{
+    int min = numbers[0];
+
+    for (int i = 1; i < array_length; i++)
+    {
+        if (min > numbers[i])
+        {
+            min = numbers[i];
+        }
+    }
+
+    return min;
+}
+
+int max(int numbers[], int array_length)
+{
+    int max = numbers[0];
+
+    for (int i = 1; i < array_length; i++)
+    {
+        if (max < numbers[i])
+        {
+            max = numbers[i];
+        }
+    }
+
+    return max;
 }
