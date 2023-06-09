fd2a905851e8e071e31d712e26254717c890eb4d
TomasNagy <tomi.nagy@outlook.com>
2023-06-09 19:45:14 +0200

    feat: implement user input

--- find_min_max.c
+++ find_min_max.c
@@ -3,5 +3,13 @@
 
 int main(int argc, char *argv[])
 {
+    const int elements = get_int("Number of elements: ");
+
+    int numbers[elements];
+
+    for (int i = 0; i < elements; i++)
+    {
+        numbers[i] = get_int("Element %i: ", i+1);
+    }
     
 }
